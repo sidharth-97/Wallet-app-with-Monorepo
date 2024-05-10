b216e96b52959ca8bac52ca2281fd9203a7d7b41
Sidharth PT <sidharthanand73@gmail.com>
2024-05-10 20:56:15 +0530

    feat: Navbar with next-auth

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,12 @@
-import Sample from "../pages/sample";
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import {Navbar} from "@repo/ui/navBar"
+
 export default function Page(): JSX.Element {
+  const session = useSession();
   return (
-    <main>
-    <Sample/>
-    </main>
+   <div>
+      <Navbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+   </div>
   );
 }
