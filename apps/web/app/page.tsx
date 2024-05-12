e109f2bcf725b90ba545928cfb4b552c062c3c0d
Sidharth PT <sidharthanand73@gmail.com>
2024-05-12 19:08:21 +0530

    fix: redirect based on auth

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,13 @@
-import Sample from "../pages/sample";
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation'
+import { authOptions } from "../lib/auth";
 
-export default function Page(): JSX.Element {
-  return (
-   <div>
-    <Sample/>
-   </div>
-  );
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect('/dashboard')
+  } else {
+    redirect('/api/auth/signin')
+  }
+  
 }
