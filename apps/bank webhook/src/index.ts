a9d276d13cad19f3057a1a686d6d2743a2ad741e
Sidharth PT <sidharthanand73@gmail.com>
2024-05-12 08:46:13 +0530

    feat: Add transaction for balance update

--- apps/bank webhook/src/index.ts
+++ apps/bank webhook/src/index.ts
@@ -1,7 +1,48 @@
-import express from "express"
+import express from "express";
+import db from "@repo/db/client";
 
-const app=express()
+const app = express();
 
-app.post("/",(req,res)=>{
+app.post("/", async (req, res) => {
+  const paymentInfo = {
+    token: req.body.token,
+    userId: req.body.user_identifier,
+    amount: req.body.amount,
+  };
 
+  try {
+    await db.$transaction([
+      db.balance.update({
+        where: {
+          userId: paymentInfo.userId,
+        },
+        data: {
+          amount: {
+            increment: paymentInfo.amount,
+          },
+        },
+      }),
+      db.onRampTransaction.update({
+        where: {
+          token: paymentInfo.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+
+    res.status(200).json({
+      message: "Captured",
+    });
+  } catch (error) {
+    res.status(411).json({
+      message: "Error while processing webhook",
+    });
+  }
+});
+
+app.listen(3003,()=>{
+    console.log("Webhook running");
+    
 })
