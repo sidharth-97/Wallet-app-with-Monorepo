ce63f1fd60f09697f1050c245bfb4a448c7db6ea
Sidharth PT <sidharthanand73@gmail.com>
2024-05-16 10:21:57 +0530

    test

--- apps/web/components/addMoneyCard.tsx
+++ apps/web/components/addMoneyCard.tsx
@@ -10,7 +10,7 @@ import { createOnRampTransaction } from "../lib/actions/createOnrampTrxn";
 const SUPPORTED_BANKS = [{
     name: "HDFC Bank",
     redirectUrl: "https://netbanking.hdfcbank.com"
-}, {
+}, { 
     name: "Axis Bank",
     redirectUrl: "https://www.axisbank.com/"
 }];
